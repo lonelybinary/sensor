adc72cacadad7accaccfd32ff40fa78a11886160
lonelybinary <62737884+lonelybinary@users.noreply.github.com>
2020-10-05 17:41:05 +1100

    Create multiple.ino

---
+++ DS18B20/multiple.ino
@@ -0,0 +1,37 @@
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+// Data wire is plugged into digital pin 2 on the Arduino
+#define ONE_WIRE_BUS 2
+
+// Setup a oneWire instance to communicate with any OneWire device
+OneWire oneWire(ONE_WIRE_BUS);	
+
+// Pass oneWire reference to DallasTemperature library
+DallasTemperature sensors(&oneWire);
+
+int deviceCount = 0;
+float tempC;
+
+void setup(void)
+{
+  sensors.begin();	// Start up the library
+  Serial.begin(9600);
+  
+  // locate devices on the bus
+  Serial.print("Locating devices...");
+  Serial.print("Found ");
+  deviceCount = sensors.getDeviceCount();
+  Serial.print(deviceCount, DEC);
+  Serial.println(" devices.");
+  Serial.println("");
+}
+
+void loop(void)
+{ 
+  // Send command to all the sensors for temperature conversion
+  sensors.requestTemperatures(); 
+  
+  // Display temperature from each sensor
+  for (int i = 0;  i < deviceCount;  i++)
+  {
