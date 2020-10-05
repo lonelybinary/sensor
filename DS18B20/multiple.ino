d1a01c55e8405693e4340f32e416c2b70816c362
lonelybinary <62737884+lonelybinary@users.noreply.github.com>
2020-10-05 17:42:15 +1100

    Update multiple.ino

--- DS18B20/multiple.ino
+++ DS18B20/multiple.ino
@@ -4,10 +4,7 @@
 // Data wire is plugged into digital pin 2 on the Arduino
 #define ONE_WIRE_BUS 2
 
-// Setup a oneWire instance to communicate with any OneWire device
 OneWire oneWire(ONE_WIRE_BUS);	
-
-// Pass oneWire reference to DallasTemperature library
 DallasTemperature sensors(&oneWire);
 
 int deviceCount = 0;
@@ -35,3 +32,18 @@ void loop(void)
   // Display temperature from each sensor
   for (int i = 0;  i < deviceCount;  i++)
   {
+    Serial.print("Sensor ");
+    Serial.print(i+1);
+    Serial.print(" : ");
+    tempC = sensors.getTempCByIndex(i);
+    Serial.print(tempC);
+    Serial.print((char)176);//shows degrees character
+    Serial.print("C  |  ");
+    Serial.print(DallasTemperature::toFahrenheit(tempC));
+    Serial.print((char)176);//shows degrees character
+    Serial.println("F");
+  }
+  
+  Serial.println("");
+  delay(1000);
+}
